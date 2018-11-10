da5897df48197443532ba10ac84edcfa6a80b2dc
Euclides Araujo <araujojr@gmail.com>
2018-11-10 18:06:42 -0400

    Overwriting BeginPlay in TankPlayerController class

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -6,3 +6,22 @@ ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>( GetPawn() );
 }
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	//UE_LOG( LogTemp, Warning, TEXT( "PlayerController Begin Play" ) );
+
+	auto ControlledTank = GetControlledTank();
+
+	if( !ControlledTank )
+	{
+		UE_LOG( LogTemp, Warning, TEXT( "PlayerController not possesing a tank" ) );
+	}
+	else
+	{
+		UE_LOG( LogTemp, Warning, TEXT( "PlayerController possesing: %s" ), *( ControlledTank->GetName() ) );
+		
+	}
+
+}
