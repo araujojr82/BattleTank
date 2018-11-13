28f55ceb87f76fb8befea7899b08b5789d723e4e
Euclides Araujo <araujojr@gmail.com>
2018-11-12 23:24:48 -0400

    Some changes to Projectile BP and Tank class, now calling SpawnActor to spawn projectiles at the Barrel's end socket

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -2,6 +2,9 @@
 
 #include "Tank.h"
 #include "TankAimingComponent.h"
+#include "Engine/World.h"
+#include "TankBarrel.h"
+#include "Projectile.h"
 
 // Sets default values
 ATank::ATank()
@@ -23,6 +26,7 @@ void ATank::BeginPlay()
 void ATank::SetBarrelReference( UTankBarrel* BarrelToSet )
 {
 	TankAimingComponent->SetBarrelReference( BarrelToSet );
+	Barrel = BarrelToSet;
 }
 
 void ATank::SetTurretReference( UTankTurret* TurretToSet )
@@ -45,5 +49,13 @@ void ATank::AimAt( FVector HitLocation )
 
 void ATank::Fire()
 {
-	UE_LOG( LogTemp, Warning, TEXT( "Fire!" ) )
+	auto Time = GetWorld()->GetTimeSeconds();
+	UE_LOG( LogTemp, Warning, TEXT( "%f: Fire!" ), Time )
+
+	if( !Barrel ) { return; }
+
+	// Spawn a projectile at the socket location on the barrel
+	GetWorld()->SpawnActor<AProjectile>( ProjectileBluePrint,
+										 Barrel->GetSocketLocation( FName( "Projectile" ) ),
+										 Barrel->GetSocketRotation( FName( "Projectile" ) ) );
 }
