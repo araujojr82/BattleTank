4600ef279a2964d1733e12c4afcfeef6332020d0
Euclides Araujo <araujojr@gmail.com>
2018-11-15 23:39:10 -0500

    Fixed collision components in Turret and Barrel and reset Projectile BP in Tank BP

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -51,14 +51,16 @@ void ATank::Fire()
 {	
 	bool isReloaded = ( GetWorld()->GetTimeSeconds() - LastFireTime ) > ReloadTimeInSeconds;
 
-	if( Barrel && isReloaded )
+	if( Barrel /*&& isReloaded*/ )
 	{
+		auto Time = GetWorld()->GetTimeSeconds();
+		UE_LOG( LogTemp, Warning, TEXT( "%f: Fire!!" ), Time )
+
 		// Spawn a projectile at the socket location on the barrel
 		auto Projectile = GetWorld()->SpawnActor<AProjectile>( ProjectileBluePrint,
 															   Barrel->GetSocketLocation( FName( "Projectile" ) ),
 															   Barrel->GetSocketRotation( FName( "Projectile" ) ) );
-
 		Projectile->LauchProjectile( LaunchSpeed );
-		LastFireTime = GetWorld()->GetTimeSeconds();
+		//LastFireTime = GetWorld()->GetTimeSeconds();
 	}
 }
