adffee05d1bd6a896a36cca3c6a827922b7178f9
Euclides Araujo <araujojr@gmail.com>
2019-04-17 19:41:13 -0400

    Fixing Tank's sideway movement by adding friction force in the Tracks

--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -24,4 +24,8 @@ public:
 	UPROPERTY( EditDefaultsOnly )
 	float TrackMaxDrivingForce = 400000; // Assume 40 Ton tank, and 1g acceleration 
 
+private:
+	UTankTrack();
+
+	virtual void TickComponent( float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction ) override;
 };
