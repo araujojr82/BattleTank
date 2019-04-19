99e56d30ccf9632f6c71f3d9d662af9450666a24
Euclides Araujo <araujojr@gmail.com>
2019-04-19 18:37:52 -0400

    Added Impact blast on projectile

--- BattleTank/Source/BattleTank/Private/Projectile.cpp
+++ BattleTank/Source/BattleTank/Private/Projectile.cpp
@@ -7,40 +7,40 @@
 AProjectile::AProjectile()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 	CollisionMesh = CreateDefaultSubobject<UStaticMeshComponent>( FName( "Collision Mesh" ) );
 	SetRootComponent( CollisionMesh );
 	CollisionMesh->SetNotifyRigidBodyCollision( true );
 	CollisionMesh->SetVisibility( false );
 
-	
-
 	LaunchBlast = CreateDefaultSubobject<UParticleSystemComponent>( FName( "Launch Blast" ) );
-	//LaunchBlast->AttachTo( RootComponent );
-	LaunchBlast->AttachToComponent( Cast< USceneComponent>( CollisionMesh ), FAttachmentTransformRules::SnapToTargetIncludingScale );
-
-	//( USceneComponent* Parent, const FAttachmentTransformRules& AttachmentRules, FName SocketName )
+	LaunchBlast->AttachToComponent( Cast< USceneComponent>( CollisionMesh ), FAttachmentTransformRules::KeepRelativeTransform );
 
 	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>( FName( "Projectile Movement" ) );
 	ProjectileMovement->bAutoActivate = false;
+
+	ImpactBlast = CreateDefaultSubobject<UParticleSystemComponent>( FName( "Impact Blast" ) );
+	ImpactBlast->AttachToComponent( Cast< USceneComponent>( CollisionMesh ), FAttachmentTransformRules::KeepRelativeTransform );
+	ImpactBlast->bAutoActivate = false;
 
 }
 
 // Called when the game starts or when spawned
 void AProjectile::BeginPlay()
 {
-	Super::BeginPlay();	
-}
-
-// Called every frame
-void AProjectile::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
+	Super::BeginPlay();
+	CollisionMesh->OnComponentHit.AddDynamic( this, &AProjectile::OnHit );	
 }
 
 void AProjectile::LauchProjectile( float Speed )
 {
 	ProjectileMovement->SetVelocityInLocalSpace( FVector::ForwardVector * Speed );
 	ProjectileMovement->Activate();
+}
+
+void AProjectile::OnHit( UPrimitiveComponent * HitComponent, AActor * OtherActor, UPrimitiveComponent * OtherComponent, FVector NormalImpulse, const FHitResult & Hit )
+{
+	LaunchBlast->Deactivate();
+	ImpactBlast->Activate();
 }
--- BattleTank/Source/BattleTank/Public/Projectile.h
+++ BattleTank/Source/BattleTank/Public/Projectile.h
@@ -22,13 +22,13 @@ public:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	// Called every frame
-	virtual void Tick( float DeltaTime ) override;
-
 	void LauchProjectile( float Speed );
 
-
 private:
+
+	UFUNCTION()
+	void OnHit( UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit );
+
 	UProjectileMovementComponent* ProjectileMovement = nullptr;
 
 	UPROPERTY( VisibleAnywhere, Category = "Components" )
@@ -36,5 +36,8 @@ private:
 
 	UPROPERTY( VisibleAnywhere, Category = "Components" )
 	UParticleSystemComponent* LaunchBlast = nullptr;
+
+	UPROPERTY( VisibleAnywhere, Category = "Components" )
+	UParticleSystemComponent* ImpactBlast = nullptr;
 
 };
